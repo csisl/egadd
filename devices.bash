472f0ae0aa469582cc9c88315191defcbf812e71
Tursko <taylor@stykocompany.com>
2018-12-29 12:32:00 -0600

    rewrote devices.bash

--- devices.bash
+++ devices.bash
@@ -2,13 +2,22 @@
 
 #This script lists the current USB devices plugged into your machine.
 for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
-    (
-        syspath="${sysdevpath%/dev}"
-        devname="$(udevadm info -q name -p $syspath)"
-        [[ "$devname" == "bus/"* ]] && continue
-        eval "$(udevadm info -q property --export -p $syspath)"
-        [[ -z "$ID_SERIAL" ]] && continue
-        echo "/dev/$devname - $ID_SERIAL"
-    )
+        
+	path="${sysdevpath%/dev}"
+        devName="$(udevadm info -q name -p $path)"
+
+        if [[ "$devName" == "bus/"* ]]; then
+		continue
+	fi
+
+     	eval "$(udevadm info -q property --export -p $path)"
+	echo $ID_SERIAL
+
+	if [[ -z "$ID_SERIAL" ]]; then
+		continue
+	else
+		echo "/dev/$devName - $ID_SERIAL"
+		ID_SERIAL=""
+	fi
 done
 
---
+++ reference/devicesOld.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#This script lists the current USB devices plugged into your machine.
+for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
+    (
+        syspath="${sysdevpath%/dev}"
+        devname="$(udevadm info -q name -p $syspath)"
+        [[ "$devname" == "bus/"* ]] && continue
+        eval "$(udevadm info -q property --export -p $syspath)"
+        [[ -z "$ID_SERIAL" ]] && continue
+        echo "/dev/$devname - $ID_SERIAL"
+    )
+done
+
