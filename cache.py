5e97890e2ad82313df89649e5a4454746d5dfa87
Tursko <taylor@stykocompany.com>
2019-01-28 14:52:05 -0600

    added comments to cache.py

--- cache.py
+++ cache.py
@@ -1,35 +1,50 @@
 #!/usr/bin/python3
 
-# This python file will handle device caching
+# This python file will handle device caching.
+# Some machines are faster / slower than others, so caching our devices
+# allows for proper confirmation of an insertion or removal. 
+# There is a 5 second timeout for checking the cache. For most modern CPUs
+# this should be plenty of time.
+
 from egadd import get_devices
 import time
 
 cache_list = []
 
-### invalidate_cache ###
-# This function iniatially sets the cache
+##### invalidate_cache #####
+# Purpose:
+#       This function resets the device cache using get_devices().
+#       This is used on the initial run of parascope.py and everytime
+#       a device is inserted/removed.
 def invalidate_cache():
     global cache_list
     cache_list = get_devices(1)
 
-### check_cache ###
-# This function compares cache with temporary list
+
+##### check_cache #####
+# Purpose:
+#       This function compares the current cache to a temp list. 
+#       get_devices() returns the list for the temp list. 
+#       When a difference is found that means that a device was properly
+#       detected or removed from the machine. 
+#       Once the difference is found, invalidate_cache() is called.
 def check_cache():
     global cache_list
     temp_list = []
 
+    # sorting the list for direct comparison
     cache_list.sort()
-    temp_list.sort()
 
+    # 5 second time out
     for i in range(0,5):
-        time.sleep(1)
+        time.sleep(1) # 1 second pause 
         temp_list = get_devices(1)
+        temp_list.sort() 
 
         if temp_list != cache_list:
-            print("found a difference!")
-            print(temp_list)
-            invalidate_cache()
+            #print("found a difference!")
+            #print(temp_list)
+            invalidate_cache() #reseting the cache
             return
+    # 5 second timeout occured            
     return
-
-
